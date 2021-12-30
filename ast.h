c164ee8d1fe1a3036b6c61c807c3b898576d9fd9
Slim Khiari <73532355+SlimKhiari@users.noreply.github.com>
2021-12-30 16:56:17 +0100

    Update ast.h

--- ast.h
+++ ast.h
@@ -13,7 +13,7 @@
 int taille_ast(char *ast);
 int index_non_terminal(char* ast, int taille_regle);
 int indice_crochet_ouvrant(char* ast, int indice_crochet_fermant_recup);
-int indice_crochet_fermant(char* ast, int indice_debut);
+int indice_crochet_fermant(char* ast, int indice_crochet_fermant_recup);
 void afficher_ast(char *ast);
 char* ast_decalage(char *ast, char caractere_a_regarder, int verif_ast_vide);
 char* ast_reduction(char* ast, int taille_regle, file_read fichier_lu,int regle);
